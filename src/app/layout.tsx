8d2014bd35c44aee300bb488dc1d2728d0225bcb
penglezos <panagiotisegl@gmail.com>
2024-06-03 22:42:24 +0300

    Keep Inter font
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto font-Roboto">
+      <body className="{inter.className} antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           {children}
         </main>
