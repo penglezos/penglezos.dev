9e854e0ccc0e26430cb4aefd99e462290c990b1f
penglezos <panagiotisegl@gmail.com>
2024-06-03 23:15:36 +0300

    Add initial social media links
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { FaGithub } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+
 export default function Home() {
   return (
     <section>
       <div className="flex justify-center items-center mb-5">
         <img src="https://avatars.githubusercontent.com/u/7030531" className="rounded-full" width="150" alt="Panagiotis Englezos"></img>
       </div>
-      <h1 className="mb-12 text-4xl font-semibold tracking-tighter text-center">Panagiotis Englezos</h1>
+      <h1 className="mb-5 text-4xl font-semibold tracking-tighter text-center">Panagiotis Englezos</h1>
+      <div className="text-3xl mb-5 flex space-x-3 justify-center">
+        <a href="mailto:panagiotisegl@gmail.com" target="_blank"><MdAlternateEmail /></a>
+        <a href="https://github.com/penglezos" target="_blank"><FaGithub /></a>
+        <a href="https://linkedin.com/in/penglezos" target="_blank"><FaLinkedin /></a>
+      </div>
     </section>
   );
 }
