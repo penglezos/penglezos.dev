c47689e5f265f141307b3e44f08193438ff962b9
penglezos <panagiotisegl@gmail.com>
2024-06-07 20:42:47 +0300

    Set icons size
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
       </div>
       <h1 className="mb-7 text-4xl font-semibold tracking-tighter text-center">Panagiotis Englezos</h1>
       <div className="text-3xl mb-5 flex space-x-3 justify-center">
-        <a href="mailto:panagiotisegl@gmail.com" target="_blank"><MdAlternateEmail /></a>
-        <a href="https://github.com/penglezos" target="_blank"><FaGithub /></a>
-        <a href="https://linkedin.com/in/penglezos" target="_blank"><FaLinkedin /></a>
-        <a href="https://paypal.me/penglezosgr" target="_blank"><FaPaypal /></a>
-        <a href="https://t.me/penglezos" target="_blank"><FaTelegram /></a>
+        <a href="mailto:panagiotisegl@gmail.com" target="_blank"><MdAlternateEmail size={35} /></a>
+        <a href="https://github.com/penglezos" target="_blank"><FaGithub size={35} /></a>
+        <a href="https://linkedin.com/in/penglezos" target="_blank"><FaLinkedin size={35} /></a>
+        <a href="https://paypal.me/penglezosgr" target="_blank"><FaPaypal size={35} /></a>
+        <a href="https://t.me/penglezos" target="_blank"><FaTelegram size={35} /></a>
       </div>
     </section>
   );
