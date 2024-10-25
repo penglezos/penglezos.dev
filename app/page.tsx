c7446ea40733d9bf782ca9e1156d9311b432a994
penglezos <panagiotisegl@gmail.com>
2024-10-25 16:04:32 +0300

    page: Reorder social profile links
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
       <h1 className="mb-5 text-4xl font-bold tracking-tighter text-center">Panagiotis Englezos</h1>
       <p className="mb-5 tracking-tighter text-center">Full-stack & Android developer</p>
       <div className="mb-10 flex space-x-4 justify-center">
-        <a href="mailto:panagiotisegl@gmail.com" target="_blank"><MdAlternateEmail size={35} /></a>
         <a href="https://github.com/penglezos" target="_blank"><FaGithub size={35} /></a>
         <a href="https://linkedin.com/in/penglezos" target="_blank"><FaLinkedin size={35} /></a>
+        <a href="mailto:panagiotisegl@gmail.com" target="_blank"><MdAlternateEmail size={35} /></a>
         <a href="https://t.me/penglezos" target="_blank"><FaTelegram size={35} /></a>
       </div>
     </section>
